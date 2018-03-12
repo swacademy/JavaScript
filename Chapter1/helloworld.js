c6591e28cc5b8a29383864017dbf28f6b618ac82
swacademy <blueexpert@hotmail.com>
2018-03-12 09:31:50 +0900

    Add files via upload

---
+++ Chapter1/helloworld.js
@@ -0,0 +1,16 @@
+﻿/*
+	filename : helloworld.js
+	function : hello
+	author : Shelley
+	hello prints out the message, "Hello,World!"
+*/
+
+function hello(){
+	//say hello to the world
+	var msg = "Hello, <em>World!</em>";
+	document.open();
+	document.write(msg);
+	document.close();
+}
+
+
